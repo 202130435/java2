9a34d884ec15fb3f68cf5d53b6786fe811968e5d
edannim <edannim121047@gmail.com>
2024-03-29 16:44:34 +0900

    0329 수업내용 추가. 배열 내용 추가

--- Ex0329.java
+++ Ex0329.java
@@ -1,5 +1,13 @@
 public class Ex0329 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        
+        for(int i = 1; i<10; i++) {
+            for(int j=1; j<10; j++) {
+                System.out.print(i+"x"+j+"="+i*j);
+                System.out.print('\t');
+            }
+        }
+        
+        System.out.println();
     }
 }
