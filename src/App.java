fd2a9f44bc7c6fc14e956bba7dd2ea97be238eb3
edannim <edannim121047@gmail.com>
2024-03-29 15:47:56 +0900

    0329 수업정리

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("월(1~12)을 입력하시오 : ");
+        int month = scanner.nextlnt();
+
+        if(3 <= month && month <= 5) {
+            System.out.println("봄");
+        }
+        else if(6 <= month && month <= 8) {
+            System.out.println("여름");
+        }
+        else if(9 <= month && month <= 11) {
+            System.out.println("가을");
+        }
+
+        else{
+            System.out.println("겨울");
+        }
+            scanner.close();
     }
 }
