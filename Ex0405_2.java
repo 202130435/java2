6d87c6aec167e6305bbcbb269bd20869bdb712e3
edannim <edannim121047@gmail.com>
2024-04-05 16:52:18 +0900

    0405 수업내용

---
+++ Ex0405_2.java
@@ -0,0 +1,26 @@
+
+public class Ex0405_2 {
+    string title;
+    string author;
+
+
+    public Ex0405_2() {
+        title = "해리포터";ㅣ author = "J K 롤링";
+    }
+    public Ex0405_2(string t) {
+        title = t; author = "작자 미상";
+    }
+    public Ex0405_2(string t, string a) {
+        title = t; author = a;
+    }
+
+    public static void main(String[] args) {
+        Ex0405_2 foo = new Ex0405_2();
+        Ex0405_2 loveStory = new Ex0405_2("춘향전");
+        Ex0405_2 littleprince = new Ex0405_2("어린 왕자", "생텍쥐페리");
+        System.out.println(foo.title+" "+foo.author);
+        System.out.println(loveStory.title+ " " + loveStory.author);
+        System.out.println(littleprince.title+ " " + littleprince.author);
+    }
+
+}
---
+++ Ex0405_3.java
@@ -0,0 +1,3 @@
+public class Ex0405_3 {
+    
+}
