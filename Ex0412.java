2e092bca3b3d9accbf0f2e592541b330aa53dae1
edannim <edannim121047@gmail.com>
2024-04-12 16:50:47 +0900

    0412 수업 내용

---
+++ Ex0412.java
@@ -0,0 +1,39 @@
+public class point {
+    private ubt x, y;
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;   
+    }
+    public void showPoint() {
+        System.out.println("("+ x +"," + y +")");
+    }
+    
+}
+
+class ColorPoint extends Point {
+    private String color;
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void showColorPoint() {
+        System.out.println(color);
+        showPoint();
+    }
+}
+
+
+public class Ex0412 {
+    
+    public static void main(String[] args) {
+        point p = new point();
+        p.set(1, 2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3, 4);
+        cp.showPoint();
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+}
